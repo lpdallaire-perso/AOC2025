ab67540a7bcbecd578c0866b66e797ff8cb287b7
lpdallaire-perso <87721804+lpdallaire-perso@users.noreply.github.com>
2025-12-02 10:34:33 -0500

    Refactor ID validation and update Solve methods
    Refactored the sequence validation functions and updated Solve1 to use new logic for identifying invalid IDs. Added functionality to Solve2 for processing input and counting valid IDs based n fragments.
    
    Fixed file parsing logic to account for the last trailing coma.

--- AdventOfCode2025/Puzzle2.cpp
+++ AdventOfCode2025/Puzzle2.cpp
@@ -5,19 +5,50 @@
 #include <algorithm>
 #include <string>
 
-bool foundInvalidSequence(long long num)
+bool findInvalidIDSplit2(long long num)
 {
-
 	std::string s = std::to_string(num);
-	size_t length = s.length();
+	size_t numLength = s.length();
 
-	if (length % 2 != 0)
+	if (numLength % 2 != 0) // can't be split in 2
 	{
 		return false;
 	}
 
-	size_t half = length / 2;
-	return s.substr(0, half) == s.substr(half, half);
+	size_t half = numLength / 2;
+	return s.substr(0, half) == s.substr(half, half); // compare the 2 halved
+}
+
+bool findInvalidIDSplitN(long long num)
+{
+	std::string s = std::to_string(num);
+	size_t numLength = s.length();
+
+	for (size_t splitSize = 1; splitSize <= numLength / 2; splitSize++)
+	{
+		if (numLength % splitSize == 0) // can be spit into that size
+		{
+			std::string split = s.substr(0, splitSize);
+			bool allMatch = true;
+
+			size_t numParts = numLength / splitSize;
+			for (int i = 1; i < numParts; i++)
+			{
+				if (s.substr(i * splitSize, splitSize) != split)
+				{
+					allMatch = false;
+					break;
+				}
+			}
+
+			if (allMatch)
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
 }
 
 void Puzzle2::Solve1()
@@ -30,9 +61,10 @@ void Puzzle2::Solve1()
 	char separator1;
 	char separator2;
 
-	while (inputFile >> firstNumber >> separator1 >> secondNumber >> separator2)
+	while (inputFile >> firstNumber >> separator1 >> secondNumber)
 	{
 		ranges.push_back({ firstNumber, secondNumber });
+		inputFile >> separator2;
 	}
 
 	long long finalCount = 0;
@@ -41,7 +73,7 @@ void Puzzle2::Solve1()
 	{
 		for (long long num = start; num <= end; num++) 
 		{
-			if (foundInvalidSequence(num))
+			if (findInvalidIDSplit2(num))
 			{
 				finalCount += num;
 			}
@@ -53,5 +85,32 @@ void Puzzle2::Solve1()
 
 void Puzzle2::Solve2()
 {
+	std::vector<std::pair<long long, long long>> ranges;
+	std::ifstream inputFile("input2.txt");
+
+	long long firstNumber;
+	long long secondNumber;
+	char separator1;
+	char separator2;
+
+	while (inputFile >> firstNumber >> separator1 >> secondNumber)
+	{
+		ranges.push_back({ firstNumber, secondNumber });
+		inputFile >> separator2;
+	}
+
+	long long finalCount = 0;
+
+	for (auto& [start, end] : ranges)
+	{
+		for (long long num = start; num <= end; num++)
+		{
+			if (findInvalidIDSplitN(num))
+			{
+				finalCount += num;
+			}
+		}
+	}
+
 	std::cout << "Puzzle 2.2" << std::endl;
 }
