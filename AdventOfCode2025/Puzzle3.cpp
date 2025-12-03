021681fe9cbe4e57f5dfa2d9b65fae23d5ccf26b
lpdallaire-perso <87721804+lpdallaire-perso@users.noreply.github.com>
2025-12-03 10:40:39 -0500

    Add logic to solve Puzzle 3.1
    Implemented logic to calculate the sum of the two highest numbers from each line in the input file for Puzzle 3.1.

--- AdventOfCode2025/Puzzle3.cpp
+++ AdventOfCode2025/Puzzle3.cpp
@@ -9,7 +9,33 @@
 void Puzzle3::Solve1()
 {
 	std::ifstream inputFile("input3.txt");
-	std::cout << "Puzzle 3.1" << std::endl;
+
+	std::string line;
+	int sum = 0;
+	while (inputFile >> line)
+	{
+		int highPos1 = 0;
+		int highPos2 = 0;
+		const char* cLine = line.c_str();
+		for (int i = 0; i < line.length()-1; i++) // find first highest number
+		{
+			if (cLine[i] > cLine[highPos1])
+				highPos1 = i;
+		}
+
+		highPos2 = highPos1 + 1;
+
+		for (int i = highPos2; i < line.length(); i++) // find first highest number
+		{
+			if (cLine[i] > cLine[highPos2])
+				highPos2 = i;
+		}
+
+		std::string finalNumber{ cLine[highPos1] , cLine[highPos2] };
+		sum += atoi(finalNumber.c_str());
+	}
+
+	std::cout << "Puzzle 3.1:" << sum << std::endl;
 }
 
 void Puzzle3::Solve2()
@@ -17,4 +43,5 @@ void Puzzle3::Solve2()
 	std::ifstream inputFile("input3.txt");
 
 	std::cout << "Puzzle 3.2" << std::endl;
+
 }
