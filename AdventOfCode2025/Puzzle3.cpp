2e0dd5d332bfebb4ac349b12c018c7d00d0ddcef
lpdallaire-perso <87721804+lpdallaire-perso@users.noreply.github.com>
2025-12-03 11:14:53 -0500

    Refactor Puzzle3 methods for long long and logic
    Updated Solve1 and Solve2 methods to use long long for sum and added logic to Solve2 for processing multiple numbers.

--- AdventOfCode2025/Puzzle3.cpp
+++ AdventOfCode2025/Puzzle3.cpp
@@ -5,13 +5,12 @@
 #include <algorithm>
 #include <string>
 
-
 void Puzzle3::Solve1()
 {
 	std::ifstream inputFile("input3.txt");
 
 	std::string line;
-	int sum = 0;
+	long long sum = 0;
 	while (inputFile >> line)
 	{
 		int highPos1 = 0;
@@ -32,7 +31,7 @@ void Puzzle3::Solve1()
 		}
 
 		std::string finalNumber{ cLine[highPos1] , cLine[highPos2] };
-		sum += atoi(finalNumber.c_str());
+		sum += std::stoll(finalNumber);
 	}
 
 	std::cout << "Puzzle 3.1:" << sum << std::endl;
@@ -42,6 +41,34 @@ void Puzzle3::Solve2()
 {
 	std::ifstream inputFile("input3.txt");
 
-	std::cout << "Puzzle 3.2" << std::endl;
+	std::string line;
+	long long sum = 0;
+	const int cellSize = 12;
+	
 
+	while (inputFile >> line)
+	{
+		std::string finalNumber;
+		int remainingNumbers = cellSize;
+		int prevHighPos = 0;
+
+		const char* cLine = line.c_str();
+		
+		for (int j = 0; j < cellSize; j++, remainingNumbers--)
+		{
+			int highPos = prevHighPos;
+
+			for (int i = prevHighPos; i < line.length() - remainingNumbers + 1; i++)
+			{
+				if (cLine[i] > cLine[highPos])
+					highPos = i;
+			}
+			finalNumber += cLine[highPos];
+			prevHighPos = highPos + 1;
+		}
+
+		sum += std::stoll(finalNumber);
+	}
+
+	std::cout << "Puzzle 3.2:" << sum << std::endl;
 }
