15ad0b1636bdc7b164216ba4fe1ac5fc37e2ee6f
lpdallaire-perso <87721804+lpdallaire-perso@users.noreply.github.com>
2025-12-06 21:13:11 -0500

    Initial commit of puzzle7 stubs

---
+++ AdventOfCode2025/Puzzle7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "Puzzle7.h"
+
+void Puzzle7::Solve1()
+{
+	std::ifstream input("input7.txt");
+
+
+	std::cout << "Puzzle 7.1: " << std::endl;
+}
+
+void Puzzle7::Solve2()
+{
+	std::ifstream input("input7.txt");
+
+	std::cout << "Puzzle 7.2: " << std::endl;
+}
---
+++ AdventOfCode2025/Puzzle7.h
@@ -0,0 +1,8 @@
+#pragma once
+class Puzzle7
+{
+public:
+
+	void Solve1();
+	void Solve2();
+};
